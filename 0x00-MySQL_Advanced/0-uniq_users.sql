27ce671f3e8a9b1b43bcfce1f0def542c40074f4
abuhanaan <adewunmi99@gmail.com>
2022-04-21 22:23:29 +0100

    First task

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,10 @@
+-- Creates a table users with the following attributes:
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+
 CREATE TABLE IF NOT EXISTS users (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email varchar(255) NOT NULL UNIQUE,
-    name varchar(255)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
 )
