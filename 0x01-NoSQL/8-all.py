8dadd3a3e6e9d676c001b4b671de4c79027e6f52
abuhanaan <adewunmi99@gmail.com>
2022-04-26 23:39:25 +0100

    fixed pycodestyle bugs

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,12 +2,13 @@
 """lists all documents in a collection
 """
 
+
 def list_all(mongo_collection):
     """lists all documents in a mongoDB collection
-    
+
     Keyword arguments:
     mongo_collection -- mongoDB collection
     Return: items in the collection
     """
-    
+
     return [doc for doc in mongo_collection.find()]
